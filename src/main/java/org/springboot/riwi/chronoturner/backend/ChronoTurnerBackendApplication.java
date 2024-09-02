a143404c47c6d7e0894b068dbb7ef211eb11f072
Santiago <santimh04@gmail.com>
2024-09-02 17:04:00 -0500

    pom.xml external libraries update

---
+++ src/main/java/org/springboot/riwi/chronoturner/backend/ChronoTurnerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.riwi.chronoturner.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoTurnerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoTurnerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springboot/riwi/chronoturner/backend/ChronoTurnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.riwi.chronoturner.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoTurnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
