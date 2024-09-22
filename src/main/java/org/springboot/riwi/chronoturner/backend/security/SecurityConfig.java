b3f8cfed67a519e5c6b41583f98887ee91c35dd5
Julian Alvarez <juliansnei@gmail.com>
2024-09-22 00:09:48 -0500

    cors interference

--- src/main/java/org/springboot/riwi/chronoturner/backend/security/SecurityConfig.java
+++ src/main/java/org/springboot/riwi/chronoturner/backend/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(
